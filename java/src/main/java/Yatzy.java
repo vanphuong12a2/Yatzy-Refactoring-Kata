a5e4290636f2a1364b12a02eb37211325a3d8d09
pnguyen <vanphuong12a2@gmail.com>
2019-03-08 08:23:25 +0100

    Refactor - chance & yatzi

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -1,22 +1,13 @@
+import java.util.Arrays;
+
 class Yatzy {
 
-    static int chance(int d1, int d2, int d3, int d4, int d5) {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    static int chance(int... dice) {
+        return Arrays.stream(dice).sum();
     }
 
     static int yatzy(int... dice) {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die - 1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
+        if (isAllDiceWithSameNumber(dice)) return 50;
         return 0;
     }
 
@@ -215,6 +206,10 @@ class Yatzy {
             return _2_at * 2 + _3_at * 3;
         else
             return 0;
+    }
+
+    private static boolean isAllDiceWithSameNumber(int... dice) {
+        return Arrays.stream(dice).distinct().count() == 1;
     }
 }
 
